4eb734cb205838bc7cb36d7d41449cd49c896d13
nannapuneni <nannapuneni@gmail.com>
2017-06-30 14:32:18 -0400

    For loop added

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,9 @@ class HelloWorld{
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
 		System.out.println("Checking in UI");
+		
+		for (int i = 0; i<10; i++){
+			System.out.println("Change time number" + i);
 			
 	}
 }
